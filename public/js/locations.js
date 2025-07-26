646fed9d03d18a712ae486e77fec6c9e0cd99461
Shaira Santos <shaisan1109@gmail.com>
2025-07-27 00:09:33 +0800

    Working brgy dropdown

--- public/js/locations.js
+++ public/js/locations.js
@@ -2,6 +2,7 @@ let form = document.forms[0]
 let regionsDropdown = form.regions
 let provincesDropdown = form.provinces
 let municipalitiesDropdown = form.municipalities
+let barangayDropdown = form.barangay
 
 // Variables changing with selection
 let jsonData
@@ -12,6 +13,8 @@ fetch('/locations')
     .then((data) => {
         jsonData = data
         getRegions(jsonData)
+
+        console.log(data)
 
         // If prefill values exist, apply them
         const prefill = {
@@ -64,7 +67,7 @@ function getProvinces() {
     output += "<option value='' index=''>Select a province</option>"
    
     // If no region is selected, disable this
-    if(regionIndex.trim() === "" || regionIndex == "") {
+    if(regionIndex.trim() === "" || regionsDropdown.disabled || regionIndex == 0) {
         provincesDropdown.disabled = true
         provincesDropdown.selectedIndex = 0
         return false
@@ -97,15 +100,15 @@ function getMunicipalities() {
 
     // Region is NCR
     if(regionIndex == 0) {
-        cities = jsonData[regionIndex].cities
-        municipalities = jsonData[regionIndex].municipalities
+        cities = jsonData[0].cities
+        municipalities = jsonData[0].municipalities // Because Pateros.
 
-        cities.forEach((city) => {
-            output += `<option value='${city.code}'>${city.name}</option>`
+        cities.forEach((city, cityIndex) => {
+            output += `<option value='${city.code}' index='${cityIndex}' munic-type='city'>${city.name}</option>`
         })
 
-        municipalities.forEach((municipality) => {
-            output += `<option value='${municipality.code}'>${municipality.name}</option>`
+        municipalities.forEach((municipality, municIndex) => {
+            output += `<option value='${municipality.code}' index='${municIndex}' munic-type='municipality'>${municipality.name}</option>`
         })
 
         municipalitiesDropdown.disabled = false
@@ -117,16 +120,68 @@ function getMunicipalities() {
         cities = jsonData[regionIndex].provinces[provinceIndex].cities
         municipalities = jsonData[regionIndex].provinces[provinceIndex].municipalities
 
-        cities.forEach((city) => {
-            output += `<option value='${city.code}'>${city.name}</option>`
+        cities.forEach((city, cityIndex) => {
+            output += `<option value='${city.code}' index='${cityIndex}' munic-type='city'>${city.name}</option>`
         })
 
-        municipalities.forEach((municipality) => {
-            output += `<option value='${municipality.code}'>${municipality.name}</option>`
+        municipalities.forEach((municipality, municIndex) => {
+            output += `<option value='${municipality.code}' index='${municIndex}' munic-type='municipality'>${municipality.name}</option>`
         })
 
         municipalitiesDropdown.disabled = false
     }
 
     municipalitiesDropdown.innerHTML = output
+}
+
+// Listen for events on municipalities dropdown
+municipalitiesDropdown.addEventListener('change', getBarangays)
+
+function getBarangays() {
+    // Index for json searching
+    let regionIndex = regionsDropdown.options[regionsDropdown.selectedIndex].getAttribute('index')
+    let provinceIndex = provincesDropdown.options[provincesDropdown.selectedIndex].getAttribute('index')
+    let municIndex = municipalitiesDropdown.options[municipalitiesDropdown.selectedIndex].getAttribute('index')
+    let municType = municipalitiesDropdown.options[municipalitiesDropdown.selectedIndex].getAttribute('munic-type')
+
+    // Initialize barangays
+    let barangays
+
+    // Initialize html
+    let output = ""
+    output += "<option value='' index=''>Select a barangay</option>"
+   
+    // If no city/municipality is selected, disable this
+    if(municIndex.trim() === "" || regionIndex.trim() === "" || municipalitiesDropdown.disabled) {
+        barangayDropdown.disabled = true
+        barangayDropdown.selectedIndex = 0
+        return false
+    } else {
+        // If region is NCR
+        if(regionIndex == 0) {
+            if(municType == 'city') { // Cities
+                if(municIndex == 5) // Manila (uses municipal districts instead)
+                    barangays = jsonData[0].cities[municIndex].municipalDistricts
+                else
+                    barangays = jsonData[0].cities[municIndex].barangays
+            }
+            else { // Pateros
+                barangays = jsonData[0].municipalities[municIndex].barangays
+            }
+
+            console.log(barangays)
+        } else {
+            if(municType == 'municipality')
+                barangays = jsonData[regionIndex].provinces[provinceIndex].municipalities[municIndex].barangays
+            else // city
+                barangays = jsonData[regionIndex].provinces[provinceIndex].cities[municIndex].barangays
+        }
+
+        barangays.forEach((barangay, barangayIndex) => {
+            output += `<option value='${barangay.code}' index='${barangayIndex}'>${barangay.name}</option>`
+        })
+        barangayDropdown.disabled = false
+    }
+
+    barangayDropdown.innerHTML = output
 }
